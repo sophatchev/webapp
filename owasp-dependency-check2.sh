caa34f27b551e1fe68b4d48b30aebbeab25b89b8
sophatchev <47827201+sophatchev@users.noreply.github.com>
2020-05-12 12:28:44 -0700

    Update owasp-dependency-check2.sh

--- owasp-dependency-check2.sh
+++ owasp-dependency-check2.sh
@@ -17,14 +17,13 @@ fi
 docker pull owasp/dependency-check
 
 docker run --rm \
-    --volume $(pwd):/:z \
-    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data:z \
-    --volume $(pwd)/"$REPORT_DIRECTORY":/report:z \
-    owasp/dependency-check:$DEP_CHECK_VERSION \
-    --noupdate \
+    --volume $(pwd):/src \
+    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data \
+    --volume "$REPORT_DIRECTORY":/report \
+    owasp/dependency-check \
     --scan /src \
     --format "ALL" \
     --project "My OWASP Dependency Check Project" \
-    --out /report    
+    --out /report
     # Use suppression like this: (/src == $pwd)
     # --suppression "/src/security/dependency-check-suppression.xml"
